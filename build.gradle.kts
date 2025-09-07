ac2d6709c26adf202be9cdb98b6349c9504cc71b
Pixoll <pixoll.gamer@gmail.com>
2025-09-06 23:47:53 -0400

    feat: setup project

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.asciidoctor)
+}
+
+group = "com.pocopi"
+version = "1.0.0"
+description = "pocopi-api"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+    // spring boot
+
+    implementation(libs.spring.boot.starter.data.jdbc)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.boot.starter.security)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.websocket)
+
+    developmentOnly(libs.spring.boot.devtools)
+    developmentOnly(libs.spring.boot.docker.compose)
+
+    annotationProcessor(libs.spring.boot.configuration.processor)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.restdocs.mockmvc)
+    testImplementation(libs.spring.security.test)
+
+    // others
+
+    implementation(libs.dotenv)
+    runtimeOnly(libs.mysql.connector)
+
+    // testing
+
+    testRuntimeOnly(libs.junit.launcher)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+    inputs.dir(project.extra["snippetsDir"]!!)
+    dependsOn(tasks.test)
+}
