d17c60c95e3701e4a63d7005ae7fe849e7b6d43e
benjag22 <benjagonzalez2022@udec.cl>
2025-09-19 01:29:57 -0300

    feat: create insert new user sql query method

--- src/main/java/com/pocopi/api/repositories/UserRepository.java
+++ src/main/java/com/pocopi/api/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.pocopi.api.repositories;
 
 import com.pocopi.api.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,19 @@ public interface UserRepository extends JpaRepository<UserModel, Integer> {
 
     @Query(value = "SELECT * from user", nativeQuery = true)
     List<UserModel> getAllUsers();
+
+    @Modifying
+    @Query(
+            value = "INSERT into user (username, group_id, anonymous, name, email, age, password)" +
+                    "values (:username,:group_id,:anonymous, :name, :email, :age, :password)"
+            , nativeQuery = true
+    )
+    void insertNewUser(@Param("username") String username,
+                       @Param("group_id") int group_id,
+                       @Param("anonymous") boolean anonymous,
+                       @Param("name") String name,
+                       @Param("email") String email,
+                       @Param("age") byte age,
+                       @Param("password") String password
+    );
 }
