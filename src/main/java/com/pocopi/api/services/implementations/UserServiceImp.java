b10cef34281f7a3c5008cb278eaf8921d387242d
benjag22 <benjagonzalez2022@udec.cl>
2025-09-19 01:31:06 -0300

    feat: add create user implementation in user service

--- src/main/java/com/pocopi/api/services/implementations/UserServiceImp.java
+++ src/main/java/com/pocopi/api/services/implementations/UserServiceImp.java
@@ -1,9 +1,15 @@
 package com.pocopi.api.services.implementations;
 
+import com.pocopi.api.dto.User.CreateUserRequest;
 import com.pocopi.api.dto.User.SingleUserResponse;
+import com.pocopi.api.models.TestGroupModel;
 import com.pocopi.api.models.UserModel;
 import com.pocopi.api.repositories.UserRepository;
+import com.pocopi.api.services.interfaces.TestGroupService;
 import com.pocopi.api.services.interfaces.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,14 +18,17 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
+    private final TestGroupService testGroupService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImp(UserRepository userRepository) {
+    @Autowired
+    public UserServiceImp(UserRepository userRepository,
+                          PasswordEncoder passwordEncoder,
+                          TestGroupService testGroupService
+    ) {
         this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserModel getUser(int id) {
-        return null;
+        this.passwordEncoder = passwordEncoder;
+        this.testGroupService = testGroupService;
     }
 
     @Override
@@ -30,5 +39,33 @@ public class UserServiceImp implements UserService {
             usersResponse.add(new SingleUserResponse(user.getUsername(), user.getName(), user.getEmail(), user.getAge()));
         }
         return usersResponse;
+    }
+
+    @Override
+    @Transactional
+    public String createUser(CreateUserRequest request) {
+        TestGroupModel group = testGroupService.getTestGroup(request.groupId());
+        System.out.println(passwordEncoder.encode(request.password()));
+
+        UserModel newUser = UserModel.builder()
+                .group(group)
+                .username(request.username())
+                .anonymous(request.anonymous())
+                .name(request.name())
+                .email(request.email())
+                .age(request.age())
+                .password(passwordEncoder.encode(request.password()))
+                .build();
+
+        userRepository.insertNewUser(
+                newUser.getUsername(),
+                newUser.getGroup().getId(),
+                newUser.isAnonymous(),
+                newUser.getName(),
+                newUser.getEmail(),
+                newUser.getAge(),
+                newUser.getPassword()
+        );
+        return "User created successfully";
     }
 }
--- src/main/java/com/pocopi/api/services/interfaces/UserService.java
+++ src/main/java/com/pocopi/api/services/interfaces/UserService.java
@@ -1,12 +1,12 @@
 package com.pocopi.api.services.interfaces;
 
+import com.pocopi.api.dto.User.CreateUserRequest;
 import com.pocopi.api.dto.User.SingleUserResponse;
-import com.pocopi.api.models.UserModel;
 
 import java.util.List;
 
 public interface UserService {
 
-    UserModel getUser(int id);
     List<SingleUserResponse> getAll();
+    String createUser(CreateUserRequest request);
 }
