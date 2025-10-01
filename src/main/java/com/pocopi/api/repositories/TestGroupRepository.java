2dc82f03bcbda81cd98162a64c9c86d79cc6bbfb
benjag22 <benjag.gonzalez13@gmail.com>
2025-10-01 15:35:31 -0300

    feat(test_group): create sql query

--- src/main/java/com/pocopi/api/repositories/TestGroupRepository.java
+++ src/main/java/com/pocopi/api/repositories/TestGroupRepository.java
@@ -2,6 +2,36 @@ package com.pocopi.api.repositories;
 
 import com.pocopi.api.models.TestGroupModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TestGroupRepository extends JpaRepository<TestGroupModel, Integer> {
+    @Query(value = """
+        SELECT
+            tg.config_version,
+            tg.label as group_label,
+            tg.probability,
+            tg.greeting,
+            tph.order as phase_order,
+            tq.order as question_order,
+            tq.image_id as question_image_id,
+            topt.order as option_order,
+            topt.text as option_text,
+            topt.image_id as option_image_id,
+            topt.correct
+        FROM test_group tg
+                 LEFT JOIN test_protocol tp ON tg.id = tp.group_id
+                 LEFT JOIN test_phase tph ON tp.id = tph.protocol_id
+                 LEFT JOIN test_question tq ON tph.id = tq.phase_id
+                 LEFT JOIN test_option topt ON tq.id = topt.question_id
+        WHERE tg.config_version = :configVersion
+        ORDER BY
+            tg.config_version,
+            tg.label,
+            tph.order,
+            tq.order,
+            topt.order
+        """,nativeQuery = true)
+    List<Object> findAllGroupsDataByConfigVersion(int configVersion);
 }
