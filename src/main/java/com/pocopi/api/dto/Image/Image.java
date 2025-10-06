ad72c0cce45a79706211eb8e14bf96bfa9badbc9
benjag22 <benjag.gonzalez13@gmail.com>
2025-10-06 01:12:27 -0300

    feat(image): add requires types to image dto

--- src/main/java/com/pocopi/api/dto/Image/Image.java
+++ src/main/java/com/pocopi/api/dto/Image/Image.java
@@ -1,7 +1,12 @@
 package com.pocopi.api.dto.Image;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record Image(
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     String url,
+
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     String alt
 ) {
 }
