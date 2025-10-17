ca180657c3e64bfe38492ec6b50fb4f217a0e929
benjag22 <benjag.gonzalez13@gmail.com>
2025-10-17 02:34:52 -0300

    !feat: remove outdated dto

--- src/main/java/com/pocopi/api/controllers/ConfigController.java
+++ src/main/java/com/pocopi/api/controllers/ConfigController.java
@@ -1,8 +1,4 @@
 package com.pocopi.api.controllers;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pocopi.api.dto.Config.PatchLastConfig;
 import com.pocopi.api.dto.Config.PatchRequest;
 import com.pocopi.api.dto.Config.PatchResponse;
 import com.pocopi.api.dto.Config.SingleConfigResponse;
@@ -11,11 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/config")
@@ -35,50 +26,8 @@ public class ConfigController {
     }
 
     @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<PatchResponse> updateConfig(
-        @RequestPart(value = "appIcon", required = false) MultipartFile appIcon,
-        @RequestParam("preTestFormQuestionOptionsFiles") MultipartFile[] preTestFiles,
-        @RequestParam("preTestFormQuestionOptionsIndexes") String preTestIndexesJson,
-        @RequestParam("postTestFormQuestionOptionsFiles") MultipartFile[] postTestFiles,
-        @RequestParam("postTestFormQuestionOptionsIndexes") String postTestIndexesJson,
-        @RequestParam("groupQuestionOptionsFiles") MultipartFile[] groupFiles,
-        @RequestParam("groupQuestionOptionsIndexes") String groupIndexesJson,
-        @RequestParam("informationCardFiles") MultipartFile[] cardFiles,
-        @RequestParam("informationCardIndexes") String cardIndexesJson,
-        @RequestPart("updateLastConfig") String updateLastConfigJson
-    ) throws JsonProcessingException, JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        Integer[] preTestIndexes = mapper.readValue(preTestIndexesJson, Integer[].class);
-        Integer[] postTestIndexes = mapper.readValue(postTestIndexesJson, Integer[].class);
-        Integer[] groupIndexes = mapper.readValue(groupIndexesJson, Integer[].class);
-        Integer[] cardIndexes = mapper.readValue(cardIndexesJson, Integer[].class);
-
-        Map<Integer, MultipartFile> preTestMap = buildFileMap(preTestFiles, preTestIndexes);
-        Map<Integer, MultipartFile> postTestMap = buildFileMap(postTestFiles, postTestIndexes);
-        Map<Integer, MultipartFile> groupMap = buildFileMap(groupFiles, groupIndexes);
-        Map<Integer, MultipartFile> cardMap = buildFileMap(cardFiles, cardIndexes);
-
-        PatchLastConfig config = mapper.readValue(updateLastConfigJson, PatchLastConfig.class);
-
-        PatchRequest request = new PatchRequest(
-            Optional.ofNullable(appIcon),
-            preTestMap,
-            postTestMap,
-            groupMap,
-            cardMap,
-            config
-        );
-
+    public ResponseEntity<PatchResponse> updateConfig(@ModelAttribute PatchRequest request){
         PatchResponse response = configService.processUpdatedConfig(request);
         return ResponseEntity.ok(response);
-    }
-
-    private Map<Integer, MultipartFile> buildFileMap(MultipartFile[] files, Integer[] indexes) {
-        Map<Integer, MultipartFile> map = new HashMap<>();
-        for (int i = 0; i < files.length; i++) {
-            map.put(indexes[i], files[i]);
-        }
-        return map;
     }
 }
